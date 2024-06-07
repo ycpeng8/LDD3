316145685768b19a55d77cb08aa8b3b18dbdca79
ycpeng-win <ycpeng@bu.edu>
2024-06-07 05:13:50 -0400

    add print process

--- hello.c
+++ hello.c
@@ -4,11 +4,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>
 MODULE_LICENSE("Dual BSD/GPL");
+
+static void print_process_id(void) 
+{
+    printk(KERN_INFO "The process is \"%s\" (pid %i)\n",
+            current->comm, current->pid);
+}
 
 static int hello_init(void)
 {
     printk(KERN_ALERT "Hello, world\n");
+    print_process_id();
     return 0;
 }
 
