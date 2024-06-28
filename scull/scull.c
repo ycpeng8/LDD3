ba6223cb8d997295e0a9580c0eba4b86897e6f52
ycpeng-win <ycpeng@bu.edu>
2024-06-28 17:51:12 +0800

    scull declare basic file operations functions

--- scull/scull.c
+++ scull/scull.c
@@ -1,14 +1,50 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/types.h>
+#include <linux/fs.h>
 
 #include "scull.h"
 
-int scull_major = 0;
+int scull_major = SCULL_MAJOR;
+int scull_minor = SCULL_MINOR;
+int scull_nr_devs = SCULL_NR_DEVS;
 
+/* module parameters injections */
 module_param(scull_major, int, S_IRUGO);
+module_param(scull_minor, int, S_IRUGO);
+module_param(scull_nr_devs, int, S_IRUGO);
+
+/* file operations connection */
+struct file_operations scull_fops = {
+	.owner = 	THIS_MODULE,
+	.llseek =	scull_llseek,
+	.read =		scull_read,
+	.write = 	scull_write,
+	.ioctl = 	scull_ioctl,
+	.open =		scull_open,
+	.release =	scull_release,
+};
 
 static int __init scull_init(void)
 {
+	int result;
+	dev_t dev = 0;
+
+	/* Set major and minor numbers */
+	if (scull_major) {
+		dev = MKDEV(scull_major, scull_minor);
+		result = register_chrdev_region(dev, scull_nr_devs, "scull");
+	} else {
+		result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs,
+			"scull");
+		scull_major = MAJOR(dev);
+	}
+
+	if (result < 0) {
+		printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
+		return result;
+	}
+
 	return 0;
 }
 
--- scull/scull.h
+++ scull/scull.h
@@ -2,12 +2,35 @@
 #define _SCULL_H_
 
 #ifndef SCULL_MAJOR
-#define SCULL_MAJOR 0			// dynamiclly assign major number
+#define SCULL_MAJOR 0			/* dynamiclly assign major number */
 #endif // !SCULL_MAJOR
+
+#ifndef SCULL_MINOR
+#define SCULL_MINOR 0           /* assign minor number */
+#endif
+
+#ifndef SCULL_NR_DEVS
+#define SCULL_NR_DEVS 4         /* scull0 ~ scull3 */
+#endif
 
 /*
  * configurable parameters
  */
 extern int scull_major;
+extern int scull_minor;
+extern int scull_nr_devs;
+
+/*
+ * file operations functions
+ */
+loff_t scull_llseek(struct file* filp, loff_t off, int whence);
+ssize_t scull_read(struct file* flip, char __user* buf, size_t count,
+                    loff_t* f_pos);
+ssize_t scull_write(struct file* flip, const char __user* buf, size_t count,
+                    loff_t* f_pos);
+int scull_ioctl(struct inode* inode, struct file* flip, unsigned int cmd,
+                unsigned long arg);
+int scull_open(struct inode* inode, struct file* flip);
+int scull_release(struct inode* inode, struct file* flip);
 
 #endif // !_SCULL_H_
